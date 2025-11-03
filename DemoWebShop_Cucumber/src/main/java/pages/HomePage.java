429c6bce05e9e6189ed8fd701091eb43877baf49
Gholami-Bahareh <baharehgholamii86@gmail.com>
2025-11-03 22:27:46 +0100

    cucumber start...

--- DemoWebShop_Cucumber/src/main/java/pages/HomePage.java
+++ DemoWebShop_Cucumber/src/main/java/pages/HomePage.java
@@ -1,5 +1,30 @@
 package pages;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class HomePage {
 
+	WebDriver driver;
+	WebDriverWait wait; 
+	
+	//Constructor
+	public HomePage(WebDriver driver ) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));  
+        }
+	
+	//Locators
+	By logoutLink = By.cssSelector(".ico-logout");
+	
+	//Methods
+	  public boolean isLogoutVisible() {
+	        return wait.until(ExpectedConditions.visibilityOfElementLocated(logoutLink)).isDisplayed();
+	    }
+	
 }
+
